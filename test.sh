3b259cc274494441671e408b764ae85c81a9845b
Nikhil Nawgiri <the@realest.ninja>
2022-03-19 21:45:13 +0100

    Refactor

--- test.sh
+++ test.sh
@@ -1,5 +1,22 @@
 MENU="rofi -i -dmenu -u 1,2,3,4"
 
+function get_date_string_for_filename(){
+	# printf -v date '%(%Y-%m-%d)T\n' -1
+}
+
+function call_ffmpeg(){
+	# $1 -> resolution
+	# $2 -> screen position
+	# ffmpeg -video_size $1 -framerate 25 -f x11grab -i :0.0+${2} scripted.mp4
+	get_date_string_for_filename
+}
+
+function get_resolution_and_position_as_array(){
+	xrectsel_string=$(xrectsel)
+	IFS='+'
+	read -a $1 <<< "$xrectsel_string"
+}
+
 start_recording="Fullscreen"
 record_rectangle="Selected area"
 
@@ -15,14 +32,9 @@ case "$CHOICE" in
 	ffmpeg -video_size $resolution -framerate 25 -f x11grab -i :0.0 fullscreen_scripted.mp4
 	;;
     $record_rectangle)
-	xrectsel_string=$(xrectsel)
-	echo $xrectsel_string
-	IFS='+'
-	read -a strarr <<< "$xrectsel_string"
-	resolution=${strarr[0]}
-	position="${strarr[1]}","${strarr[2]}"
-	echo $resolution
-	echo $position
-	ffmpeg -video_size $resolution -framerate 25 -f x11grab -i :0.0+${position} scripted.mp4
+	get_resolution_and_position_as_array resolution_and_position
+	resolution=${resolution_and_position[0]}
+	position="${resolution_and_position[1]}","${resolution_and_position[2]}"
+	call_ffmpeg $resolution $position
 	;;
 esac
